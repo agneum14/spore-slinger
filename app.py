ef8f9a6c01e338bce8ff36ec2cb40df2a34d905f
agneum14 <agneum14@gmail.com>
2023-04-06 23:26:52 -0400

    add /edit command so users can modify their library

--- app.py
+++ app.py
@@ -24,6 +24,16 @@ jingle_ids: list[int] = []
 for jingle in listdir("jingles"):
     jingle_ids.append(int(jingle.split(".")[0]))
 
+# misc
+cats = ["cubensis", "albino cubensis", "other psylocybe", "panaelous", "gourmet"]
+cat_descs = [
+    "Psilocybe Cubensis",
+    "Albino Psilocybe Cubensis",
+    "Other Psolocybes",
+    "Panaelous",
+    "Gourmet",
+]
+
 
 @bot.event
 async def on_ready():
@@ -44,6 +54,79 @@ async def canned_respond(ctx: discord.ApplicationContext):
 get_cat_strains = lambda category: scol.find({"category": category}).sort("name", 1)
 
 
+# add new trader if ID not in category
+def assert_trader(id: int):
+    if not [trader := tcol.find_one({"_id": id})]:
+        data = {"_id": id, "strains": [], "whitelist_enabled": False, "whitelist": []}
+        tcol.insert_one(data)
+        trader = tcol.find_one({"_id": id})
+
+    return trader
+
+
+# Button subclass for /edit command
+class EditButton(discord.ui.Button):
+    def __init__(self, label, style, sid, trader):
+        super().__init__(style=style, label=label)
+        self.sid = sid
+        self.tid = trader["_id"]
+        self.tstrains = trader["strains"]
+
+    async def callback(self, interaction):
+        await interaction.response.defer()
+
+        if self.sid in self.tstrains:
+            self.tstrains.remove(self.sid)
+            self.style = discord.ButtonStyle.red
+            tcol.update_one({"_id": self.tid}, {"$set": {"strains": self.tstrains}})
+        else:
+            self.tstrains.append(self.sid)
+            self.style = discord.ButtonStyle.green
+            tcol.update_one({"_id": self.tid}, {"$set": {"strains": self.tstrains}})
+
+        await interaction.edit_original_response(view=self.view)
+
+
+# /edit: user edits their strain list via clickable buttons
+@bot.application_command(description="Edit your inventory, like a lewd MMORPG")
+async def edit(ctx: discord.ApplicationContext):
+    await canned_respond(ctx)
+
+    cat_views = []
+    trader = assert_trader(ctx.author.id)
+
+    for cat in cats:
+        views: list[discord.ui.View] = []
+
+        view = discord.ui.View()
+        for i, strain in enumerate(get_cat_strains(cat)):
+            if i % 25 == 0 and i != 0:
+                views.append(view)
+                view = discord.ui.View()
+
+            label = strain["name"]  # type: ignore
+            style = (
+                discord.ButtonStyle.green
+                if strain["_id"] in trader["strains"]  # type: ignore
+                else discord.ButtonStyle.red
+            )
+            strain_id = strain["_id"]
+            button = EditButton(label, style, strain_id, trader)
+            view.add_item(button)
+        views.append(view)
+
+        cat_views.append(views)
+
+    async def send_cat_buttons(desc, cat_view):
+        await ctx.author.send(f"**{desc}**")
+        for view in cat_view:
+            await ctx.author.send(view=view)
+
+    await ctx.author.send("**BE PATIENT: DISCORD LIMITS MESSAGE FREQUENCY**")
+    for desc, cat_view in zip(cat_descs, cat_views):
+        await send_cat_buttons(desc, cat_view)
+
+
 # /handled: list currently handled strains
 @bot.application_command(description="List strains currently handled by Spore Slinger")
 async def handled(ctx: discord.ApplicationContext):
@@ -59,11 +142,13 @@ async def handled(ctx: discord.ApplicationContext):
         return msg
 
     msg = "**CURRENTLY HANDLED STRAINS:**\n"
-    msg += get_cat_msg("Psilocybe Cubensis", "cubensis")
-    msg += get_cat_msg("Albino Psilocybe Cubensis", "albino cubensis")
-    msg += get_cat_msg("Other Psilocybes", "other psylocybe")
-    msg += get_cat_msg("Psilocybe Panaelous", "panaelous")
-    msg += get_cat_msg("Gourmets", "gourmet")
+    for desc, cat in zip(cat_descs, cats):
+        msg += get_cat_msg(desc, cat)
+    # msg += get_cat_msg("Psilocybe Cubensis", "cubensis")
+    # msg += get_cat_msg("Albino Psilocybe Cubensis", "albino cubensis")
+    # msg += get_cat_msg("Other Psilocybes", "other psylocybe")
+    # msg += get_cat_msg("Psilocybe Panaelous", "panaelous")
+    # msg += get_cat_msg("Gourmets", "gourmet")
     await ctx.author.send(msg[:-1])
 
 
